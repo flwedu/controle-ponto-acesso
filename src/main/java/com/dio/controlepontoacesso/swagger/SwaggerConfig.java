2fdf6c2c6ba683609ae1293187c2a49c98050bbe
Eduardo Aquino <55285816+flwedu@users.noreply.github.com>
2021-07-18 17:58:15 -0300

    quebra de linhas

--- src/main/java/com/dio/controlepontoacesso/swagger/SwaggerConfig.java
+++ src/main/java/com/dio/controlepontoacesso/swagger/SwaggerConfig.java
@@ -2,8 +2,8 @@ package com.dio.controlepontoacesso.swagger;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -12,28 +12,24 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
-    @Value("${application.title_description}") String title_description;
-    @Value("${application.version}") String version;
-    @Value("${application.description}") String description;
+    @Value("${application.title_description}")
+    String title_description;
+    @Value("${application.version}")
+    String version;
+    @Value("${application.description}")
+    String description;
 
     @Bean
-    public Docket customApi(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
+    public Docket customApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
 
-                .apis(RequestHandlerSelectors.basePackage("br.dio.controlepontoacesso"))
-                .build().apiInfo(infoApi());
+                .apis(RequestHandlerSelectors.basePackage("br.dio.controlepontoacesso")).build().apiInfo(infoApi());
     }
 
-    private ApiInfo infoApi(){
-        return new ApiInfoBuilder()
-                .title(title_description)
-                .description(description)
-                .version(version)
-                .license("Apache License Version 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+    private ApiInfo infoApi() {
+        return new ApiInfoBuilder().title(title_description).description(description).version(version)
+                .license("Apache License Version 2.0").licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
                 .build();
     }
-
 
 }
