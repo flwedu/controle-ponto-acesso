c49cc3b412e4ff54e135635ff7683638538bd4df
Eduardo Aquino <55285816+flwedu@users.noreply.github.com>
2021-07-18 12:18:02 -0300

    Criação de controller para TipoData

---
+++ src/main/java/com/dio/controlepontoacesso/controller/TipoDataController.java
@@ -0,0 +1,49 @@
+package com.dio.controlepontoacesso.controller;
+
+import com.dio.controlepontoacesso.model.TipoData;
+import com.dio.controlepontoacesso.service.TipoDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tipodata")
+public class TipoDataController {
+
+    @Autowired
+    TipoDataService service;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<TipoData>> getListaTipoData(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<TipoData> getTipoDataById(@PathVariable long id){
+        return service.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<TipoData> saveJornada(@RequestBody TipoData tipoData){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.save(tipoData));
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<TipoData> putJornada(@RequestBody TipoData tipoData){
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(service.save(tipoData));
+    }
+
+    @DeleteMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<TipoData> deleteJornada(@NotNull @RequestBody TipoData tipoData){
+        service.delete(tipoData.getId());
+        return ResponseEntity.ok().build();
+    }
+}
